4b941b2b129d26bf67a96c84273e65dfa6e09bd9
padillakaye213-cell <padillakaye213@gmail.com>
2026-09-16 12:06:18 +0800

    Add files via upload

---
+++ sunsonsolar.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 16, 2026 at 05:52 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sunsonsolar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration form`
+--
+
+CREATE TABLE `registration form` (
+  `user_id` int(11) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `middle name` varchar(100) NOT NULL,
+  `last name` varchar(100) NOT NULL,
+  `birthdate` date NOT NULL,
+  `gender` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone number` varchar(20) NOT NULL,
+  `address` text NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `registration form`
+--
+ALTER TABLE `registration form`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `registration form`
+--
+ALTER TABLE `registration form`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
